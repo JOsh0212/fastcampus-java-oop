180c77460c522a9b297c317907a99547ec24cdfd
JOsh0212 <0869070@gmail.com>
2024-01-04 14:37:34 +0900

    [fix][#3] 정렬 구현체를 사용하는 서비스를 별도로 작성, 의존성 주입 방법 적용

---
+++ src/main/java/service/SortService.java
@@ -0,0 +1,20 @@
+package service;
+
+import logic.JavaSort;
+import logic.Sort;
+
+import java.util.List;
+
+public class SortService {
+
+    private final Sort<String> sort;    //제어의 역전 - 생성자에서 한번만 주입
+
+    public SortService(Sort<String> sort) {
+        this.sort = sort;
+        System.out.println("구현체 : " + sort.getClass().getName());
+    }
+
+    public List<String> doSort(List<String> list){
+        return sort.sort(list);     // 이 코드는 어떻게 정렬이 실행되는지 알 수 없음
+    }
+}
